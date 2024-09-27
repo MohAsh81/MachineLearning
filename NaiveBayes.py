ec2c04138f40951a18c4ae24e22d2ca18f91dd9e
Mohammad <mohammad.m.osati@gmail.com>
2024-09-27 20:05:02 +0330

    Naive bayes file added

---
+++ NaiveBayes.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.preprocessing import LabelEncoder
+
+file = pd.read_csv("titanic.csv")
+file.drop(['PassengerId', 'Name', 'SibSp', 'Parch',
+           'Ticket', 'Cabin', 'Embarked'], axis='columns', inplace=True)
+
+target = file.Survived
+inputs = file.drop("Survived", axis='columns')
+
+le = LabelEncoder()
+inputs["Sex"] = le.fit_transform(inputs['Sex'])
+
+inputs.Age = inputs.Age.fillna(round(inputs.Age.mean(),0))
+
+x_train,x_test,y_train,y_test = train_test_split(inputs,target,test_size=0.2)
+
+model = GaussianNB().fit(x_train,y_train)
+print(model.score(x_test,y_test))
